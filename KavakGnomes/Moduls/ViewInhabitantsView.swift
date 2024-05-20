6ead37a0bcae4e1b206fc9b2d1c6f82b9e779434
daniel ortiz millan <danielortizmillan@MacBook-Air-de-daniel.local>
2024-05-20 15:50:08 -0600

    search and cancel

--- KavakGnomes/Moduls/ViewInhabitantsView.swift
+++ KavakGnomes/Moduls/ViewInhabitantsView.swift
@@ -7,9 +7,117 @@
 
 import SwiftUI
 
+struct HabitantesView: View {
+    @State private var gnomes: [Brastlewark] = []
+    @State private var searchText: String = ""
+    
+    var filteredGnomes: [Brastlewark] {
+        if searchText.isEmpty {
+            return gnomes
+        } else {
+            return gnomes.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+        }
+    }
+    
+    var body: some View {
+            NavigationView {
+                VStack {
+                    HStack {
+                        TextField("Search...", text: $searchText)
+                            .padding(7)
+                            .padding(.horizontal, 25)
+                            .background(Color(.systemGray6))
+                            .cornerRadius(8)
+                            .overlay(
+                                HStack {
+                                    Image(systemName: "magnifyingglass")
+                                        .foregroundColor(.gray)
+                                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                        .padding(.leading, 8)
+                                }
+                            )
+                            .padding(.horizontal, 10)
+                        
+                        if !searchText.isEmpty {
+                            Button(action: {
+                                self.searchText = ""
+                            }) {
+                                Text("Cancel")
+                                    .foregroundColor(.blue)
+                            }
+                            .padding(.trailing, 10)
+                            .transition(.move(edge: .trailing))
+                        }
+                    }
+                    .animation(.default)
+                    
+                    List(filteredGnomes, id: \.id) { gnome in
+                        GnomeRow(gnome: gnome)
+                    }
+                    .onAppear {
+                        fetchGnomes()
+                    }
+                    .navigationTitle("Habitantes")
+                }
+                .padding()
+            }
+        }
+    
+    func fetchGnomes() {
+        GnomesApiClient().getGnomes { result in
+            switch result {
+            case .success(let gnomesModel):
+                DispatchQueue.main.async {
+                    self.gnomes = gnomesModel.brastlewark
+                }
+            case .failure(let error):
+                print("Error fetching gnomes: \(error)")
+            }
+        }
+    }
+}
+
+struct GnomeRow: View {
+    let gnome: Brastlewark
+    
+    var body: some View {
+        NavigationLink(destination: GnomeDetailView(gnome: gnome)) {
+            HStack {
+                AsyncImage(url: URL(string: gnome.thumbnail)) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 80)
+                        .clipShape(Circle())
+                } placeholder: {
+                    ProgressView()
+                }
+                Text("Name: \(gnome.name)")
+                    .font(.headline)
+                Spacer()
+            }
+            .padding()
+        }
+    }
+}
+
+
+
+
+
+/*import SwiftUI
 
 struct HabitantesView: View {
     @State private var gnomes: [Brastlewark] = []
+    @State private var searchText: String = ""
+    
+    var filteredGnomes: [Brastlewark] {
+            if searchText.isEmpty {
+                return gnomes
+            } else {
+                return gnomes.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+            }
+        }
     
     var body: some View {
         NavigationView {
@@ -39,21 +147,18 @@ struct HabitantesView: View {
 
 struct GnomeRow: View {
     let gnome: Brastlewark
-    
     var body: some View {
         NavigationLink(destination: GnomeDetailView(gnome: gnome)) {
             HStack {
                 AsyncImage(url: URL(string: gnome.thumbnail)) { image in
-                                    image
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 70, height: 70)
-                                        .clipShape(Circle())
-                                } placeholder: {
-                                    ProgressView()
-                                }
-
-
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 70, height: 70)
+                        .clipShape(Circle())
+                } placeholder: {
+                    ProgressView()
+                }
                 Text("Name: \(gnome.name)")
                     .font(.headline)
                 Spacer()
@@ -61,7 +166,7 @@ struct GnomeRow: View {
             .padding()
         }
     }
-}
+}*/
 
 #Preview {
     HabitantesView()
